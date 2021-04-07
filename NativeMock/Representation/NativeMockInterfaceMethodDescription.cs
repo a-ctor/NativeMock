44ff850441ee025ce0bccd436b865fee1af470c3
Widauer Patrick <a__ctor@hotmail.com>
2021-04-07 20:08:06 +0200

    Remove unused NativeMockCallback

--- NativeMock/Representation/NativeMockInterfaceMethodDescription.cs
+++ NativeMock/Representation/NativeMockInterfaceMethodDescription.cs
@@ -5,8 +5,5 @@ namespace NativeMock.Representation
   /// <summary>
   /// A description of a mock interface method.
   /// </summary>
-  internal record NativeMockInterfaceMethodDescription(NativeFunctionIdentifier Name, MethodInfo InterfaceMethod, MethodInfo StubTargetMethod, NativeMockBehavior Behavior)
-  {
-    public NativeMockCallback CreateCallback (object? target) => new (target, InterfaceMethod);
-  }
+  internal record NativeMockInterfaceMethodDescription(NativeFunctionIdentifier Name, MethodInfo InterfaceMethod, MethodInfo StubTargetMethod, NativeMockBehavior Behavior);
 }
