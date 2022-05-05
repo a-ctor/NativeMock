1ae7ecb02817f2e5b2e6fb17f12e97734048cc44
Widauer Patrick <a__ctor@hotmail.com>
2022-05-05 19:34:26 +0200

    Fix roslyn analyzer build failure due to target-typed new

--- src/NativeMock.Analyzer/Shared/Rules.cs
+++ src/NativeMock.Analyzer/Shared/Rules.cs
@@ -7,13 +7,10 @@ namespace NativeMock.Analyzer.Shared
   {
     private const string c_designCategory = "Design";
 
-    public static readonly DiagnosticDescriptor NoDeclaringTypeRule = new (
+    public static readonly DiagnosticDescriptor NoDeclaringTypeRule = new DiagnosticDescriptor (
       RuleIds.NoDeclaringTypeRuleId,
       new LocalizableResourceString (nameof(Resources.NoDeclaringTypeTitle), Resources.ResourceManager, typeof(Resources)),
-      new LocalizableResourceString (nameof(Resources.NoDeclaringTypeMessageFormat), Resources.ResourceManager, typeof(Resources)),
-      c_designCategory,
-      DiagnosticSeverity.Error,
-      true,
+      new LocalizableResourceString (nameof(Resources.NoDeclaringTypeMessageFormat), Resources.ResourceManager, typeof(Resources)), c_designCategory, DiagnosticSeverity.Error, true,
       new LocalizableResourceString (nameof(Resources.NoDeclaringTypeDescription), Resources.ResourceManager, typeof(Resources)));
 
     public static Diagnostic CreateNoDeclaringTypeDiagnostic (IMethodSymbol method, NativeFunctionIdentifier identifier, ITypeSymbol declaringType)
@@ -26,13 +23,10 @@ namespace NativeMock.Analyzer.Shared
         declaringType.ToDisplayString (SymbolDisplayFormat.CSharpShortErrorMessageFormat));
     }
 
-    public static readonly DiagnosticDescriptor DeclaringFunctionSignatureMismatchRule = new (
+    public static readonly DiagnosticDescriptor DeclaringFunctionSignatureMismatchRule = new DiagnosticDescriptor (
       RuleIds.DeclaringFunctionSignatureMismatchRuleId,
       new LocalizableResourceString (nameof(Resources.DeclaringFunctionSignatureMismatchTitle), Resources.ResourceManager, typeof(Resources)),
-      new LocalizableResourceString (nameof(Resources.DeclaringFunctionSignatureMismatchMessageFormat), Resources.ResourceManager, typeof(Resources)),
-      c_designCategory,
-      DiagnosticSeverity.Error,
-      true,
+      new LocalizableResourceString (nameof(Resources.DeclaringFunctionSignatureMismatchMessageFormat), Resources.ResourceManager, typeof(Resources)), c_designCategory, DiagnosticSeverity.Error, true,
       new LocalizableResourceString (nameof(Resources.DeclaringFunctionSignatureMismatchDescription), Resources.ResourceManager, typeof(Resources)));
 
     public static Diagnostic CreateDeclaringFunctionSignatureMismatchDiagnostic (IMethodSymbol method, IMethodSymbol declaringMethod)
